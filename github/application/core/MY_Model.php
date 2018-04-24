0f184b12b2a909e33909677f779a5858284d495e
LE THI HUE <thihue@cubevn.local>
2018-04-24 12:58:53 +0700

    ccc

--- github/application/core/MY_Model.php
+++
@@ -1,5 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class MY_Model extends CI_Model {
-    // Ten table
-    var $table = 'user';
-}
--- github/application/helpers/common_helper.php
+++
@@ -1,6 +0,0 @@
-<?php
-function help(){
-    $da['tit']='abc';
-    echo TEST;
-}
-define("TEST", "test data");//bien mac dinh // echo TEST => test data
