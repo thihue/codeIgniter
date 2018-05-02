94bfcfa2c2f6dc57e69ce1f30c3756483f5a558e
LE THI HUE <thihue@CV06008D-PC.cubevn.local>
2018-05-02 11:05:33 +0700

    css

--- application/views/main.php
+++ application/views/main.php
@@ -1,61 +1,79 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title><?php echo $tit; ?></title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css">
-    <script src="http://code.jquery.com/jquery-1.12.4.js" rel="stylesheet"></script>
-    <script src="http://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-    <script src="http://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js"></script>
-    <script>
-    $(document).ready(function() {
-            $('#example').DataTable();
-        });
-    </script>
-    <style>
-        /* @import url(https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css);
+	<title>
+		<?php echo $tit; ?>
+	</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css">
+	<script src="http://code.jquery.com/jquery-1.12.4.js" rel="stylesheet"></script>
+	<script src="http://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+	<script src="http://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js"></script>
+	<script>
+		$(document).ready(function () {
+			$('#example').DataTable();
+		});
+
+	</script>
+	<style>
+		/* @import url(https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css);
         @import url(https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css); */
-        table tbody{
-            color:blue;
-            }
-        table thead{color:white;
-            background:black;}
-        table tfoot{color:white;
-            background:black;}
-    </style>
+
+		table tbody {
+			color: blue;
+		}
+
+		table thead {
+			color: white;
+			background: black;
+		}
+
+		table tfoot {
+			color: white;
+			background: black;
+		}
+
+	</style>
 </head>
+
 <body>
-<div class="container">
-<p class="text-center" style="padding-top: 15px;">Xin chào 
-    <span style="color: red">
-        <?php echo $this->session->userdata('login');?>
-    </span>
-</p>
-<a href="<?php echo site_url("user/logout");?>"><i class="fa fa-sign-out" aria-hidden="true"></i> Logout</a>
-<table id="example" class="table table-striped table-bordered" style="width:100%">
-        <thead>
-            <tr>
-                <td>ID</td>
-                <td>Username</td>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($list as $d){ ?>
-            <tr>  
-                <td><?php echo $d['id'] ?></td>
-                <td><?php echo $d['username'] ?></td> 
-            </tr>
-            <?php } ?>
-        </tbody>
-        <tfoot>
-            <tr>
-                <td>ID</td>
-                <td>Username</td>
-            </tr>
-        </tfoot>
-    </table>
+	<div class="container">
+		<p class="text-center" style="padding-top: 15px;">Xin chào
+			<span style="color: red">
+				<?php echo $this->session->userdata('login');?>
+			</span>
+		</p>
+		<a href="<?php echo site_url(" user/logout ");?>">
+			<i class="fa fa-sign-out" aria-hidden="true"></i> Logout</a>
+		<table id="example" class="table table-striped table-bordered" style="width:100%">
+			<thead>
+				<tr>
+					<td>ID</td>
+					<td>Username</td>
+				</tr>
+			</thead>
+			<tbody>
+				<?php foreach($list as $d){ ?>
+				<tr>
+					<td>
+						<?php echo $d['id'] ?>
+					</td>
+					<td>
+						<?php echo $d['username'] ?>
+					</td>
+				</tr>
+				<?php } ?>
+			</tbody>
+			<tfoot>
+				<tr>
+					<td>ID</td>
+					<td>Username</td>
+				</tr>
+			</tfoot>
+		</table>
 
-</div>
+	</div>
 </body>
-</html>
 
+</html>
